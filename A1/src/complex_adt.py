7b9ecb4f0252966ddbe084f1378dcb7fd189bb76
Alan Scott <scotta30@mcmaster.ca>
2021-01-13 01:34:55 -0500

    Updated variable names, added get_phi

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -6,20 +6,23 @@
 import math
 
 class ComplexT:
-	realx, complexy = 0, 0
+	x, y = 0, 0
 
 	def __init__(self,x,y):
-		self.realx = x
-		self.complexy = y
+		self.x = x
+		self.y = y
 
 	def real(self):
-		return self.realx
+		return self.x
 
 	def imag(self):
-		return self.complexy
+		return self.y
 
 	def get_r(self):
-		return math.sqrt(self.realx**2 + self.complexy**2)
+		return math.sqrt(self.x**2 + self.y**2)
+
+	def get_phi(self):
+		return 2*math.atan(self.y/(math.sqrt(self.x**2 + self.y**2)+self.x))
 
 
 
