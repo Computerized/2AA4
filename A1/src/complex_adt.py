42ebda4bbe25b740e8b985637d594b1132dd613a
Alan Scott <scotta30@mcmaster.ca>
2021-01-18 18:08:22 -0500

    Flake8 changes

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -1,59 +1,56 @@
-## @file complex_adt.py
+# @file complex_adt.py
 #  @author scotta30
-#  @brief 
+#  @brief
 #  @date 2021-01-13
 
 import math
 
+
 class ComplexT:
-	x, y = 0, 0
+    x, y = 0, 0
 
-	def __init__(self,x,y):
-		self.x = x
-		self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-	def real(self):
-		return self.x
+    def real(self):
+        return self.x
 
-	def imag(self):
-		return self.y
+    def imag(self):
+        return self.y
 
-	def get_r(self):
-		return math.sqrt(self.x**2 + self.y**2)
+    def get_r(self):
+        return math.sqrt(self.x**2 + self.y**2)
 
-	def get_phi(self):
-		return 2*math.atan(self.y/(math.sqrt(self.x**2 + self.y**2)+self.x))
+    def get_phi(self):
+        return 2*math.atan(self.y/(math.sqrt(self.x**2 + self.y**2)+self.x))
 
-	def equal(self,obj):
-		return (obj.real() == self.x) and (obj.imag() == self.y)
+    def equal(self, obj):
+        return (obj.real() == self.x) and (obj.imag() == self.y)
 
-	def conj(self):
-		return ComplexT(self.x,-self.y)
+    def conj(self):
+        return ComplexT(self.x, -self.y)
 
-	def add(self,obj):
-		return ComplexT(self.x + obj.real(), self.y + obj.imag())
+    def add(self, obj):
+        return ComplexT(self.x + obj.real(), self.y + obj.imag())
 
-	def sub(self,obj):
-		return ComplexT(self.x - obj.real(), self.y - obj.imag())
+    def sub(self, obj):
+        return ComplexT(self.x - obj.real(), self.y - obj.imag())
 
-	def mult(self,obj):
-		r = self.x * obj.real() - self.y * obj.imag()
-		i = self.x * obj.imag() + self.y * obj.real()
-		return ComplexT(r, i)
+    def mult(self, obj):
+        r = self.x * obj.real() - self.y * obj.imag()
+        i = self.x * obj.imag() + self.y * obj.real()
+        return ComplexT(r, i)
 
-	def recip(self):
-		denom = self.x ** 2 + self.y ** 2
-		return ComplexT(self.x/denom, -self.y/denom)
+    def recip(self):
+        denom = self.x ** 2 + self.y ** 2
+        return ComplexT(self.x/denom, -self.y/denom)
 
-	def div(self, obj):
-		return mult(obj.recip)
+    def div(self, obj):
+        return mult(obj.recip)
 
-	def sqrt(self):
-		r = get_r()
-		real = math.sqrt((r + self.x)/2)
-		imag = (self.y/math.abs(self.y))*math.sqrt((r - self.y)/2)
-		return ComplexT(real,imag)
-		
-
-
-
+    def sqrt(self):
+        r = get_r()
+        real = math.sqrt((r + self.x)/2)
+        imag = (self.y/math.abs(self.y))*math.sqrt((r - self.y)/2)
+        return ComplexT(real, imag)
--- A1/src/triangle_adt.py
+++ A1/src/triangle_adt.py
@@ -1,4 +1,4 @@
-## @file triangle_adt.py
+#  @file triangle_adt.py
 #  @author Alan Scott
 #  @brief Contains a class which represents a given triangle
 #  @date 01/18/2020
@@ -6,47 +6,48 @@
 import math
 from enum import Enum
 
-## @brief An ADT for a triangle represented by 3 side lengths
+
+#  @brief An ADT for a triangle represented by 3 side lengths
 #  @details Triangle defined by 3 side lengths
 class TriangleT:
-    a,b,c = 0,0,0
+    a, b, c = 0, 0, 0
 
-    ## @brief Constructor for TriangleT
+    #  @brief Constructor for TriangleT
     #  @details Represents a triangle with 3 given sides
     #  @param a An integer representing the length of the first side
     #  @param b An integer representing the length of the second side
     #  @param c An integer representing the length of the third side
-    def __init__(self,a,b,c):
-        self.a,self.b,self.c = a,b,c
+    def __init__(self, a, b, c):
+        self.a, self.b, self.c = a, b, c
 
-    ## @brief Returns the side lengths of the triangle
+    # @brief Returns the side lengths of the triangle
     #  @return The three side lengths in a tuple
     def get_sides(self):
-        return self.a,self.b,self.c
+        return self.a, self.b, self.c
 
-    ## @brief Compares the current triangle and a given triangle
+    # @brief Compares the current triangle and a given triangle
     #  @return True if the triangles are equal
     #  @param obj The triangle being compared to
-    def equal(self,obj):
-        return (self.a,self.b,self.c == obj.get_sides())
+    def equal(self, obj):
+        return (self.a, self.b, self.c == obj.get_sides())
 
-    ## @brief Sums the side lengths of all 3 sides
+    # @brief Sums the side lengths of all 3 sides
     #  @return The perimeter of the triangle
     def perim(self):
         return self.a + self.b + self.c
 
-    ## @brief Computes the area of the triangle
+    # @brief Computes the area of the triangle
     #  @return The area of the triangle
     def area(self):
         p = self.perim()/2
         return math.sqrt(p*(p - self.a)*(p - self.b)*(p - self.c))
 
-    ## @brief Determines whether the given triangle is possible in Euclidian space
+    # @brief Determines whether the given triangle is possible in Euclidian space
     #  @return True if the triangle is physically possible
     def is_valid(self):
         return (self.a + self.b > self.c) and (self.a + self.c > self.b) and (self.b + self.c > self.a) and self.a > 0 and self.b > 0 and self.c > 0
 
-    ## @brief Determines the type of the triangle
+    # @brief Determines the type of the triangle
     #  @return A TriType value representing the type of triangle
     def tri_type(self):
         if (self.a == self.b and self.a == self.c):
@@ -57,9 +58,9 @@ class TriangleT:
             return TriType.right
         return TriType.scalene
 
+
 class TriType(Enum):
     equilat = 1
     isosceles = 2
     scalene = 3
     right = 4
-        
