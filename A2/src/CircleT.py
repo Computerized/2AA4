93be6cdc4711239f3b04d115892f6fa47043ac92
Alan Scott <scotta30@mcmaster.ca>
2021-02-03 12:06:44 -0500

    Created Shape interface, created CircleT

--- A2/src/CircleT.py
+++ A2/src/CircleT.py
@@ -1,4 +1,29 @@
 ## @file CircleT.py
-#  @author
+#  @author Alan Scott
 #  @brief
-#  @date
+#  @date 11/02/21
+
+from Shape import Shape
+
+class CircleT(Shape):
+    x,y,r,m = 0.0,0.0,0.0,0.0
+
+    def __init__(self,xs,ys,rs,ms):
+        if (not (rs > 0 and ms > 0)):
+            raise Exception("ValueError")
+        self.x = xs
+        self.y = ys
+        self.r = rs
+        self.m = ms
+
+    def cm_x(self):
+        return self.x
+
+    def cm_y(self):
+        return self.y
+
+    def mass(self):
+        return self.m
+
+    def m_inert(self):
+        return (self.m*self.r**2)/2
--- A2/src/Shape.py
+++ A2/src/Shape.py
@@ -2,3 +2,20 @@
 #  @author
 #  @brief
 #  @date
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    
+    @abstractmethod
+    def cm_x(self):
+        pass
+
+    def cm_y(self):
+        pass
+
+    def mass(self):
+        pass
+
+    def m_inert(self):
+        pass
