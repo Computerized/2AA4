6eea55aaf32964a9a0fa76e58340b34e3e54e0a7
Alan Scott <scotta30@mcmaster.ca>
2021-02-03 12:20:07 -0500

    Update TriangleT.py

--- A2/src/TriangleT.py
+++ A2/src/TriangleT.py
@@ -2,3 +2,25 @@
 #  @author
 #  @brief
 #  @date
+
+from Shape import Shape
+
+class TriangleT(Shape):
+    x,y,s,m = 0.0,0.0,0.0,0.0
+
+    def __init__(self,xs,ys,ss,ms):
+        if (not (ss > 0 and ms > 0)):
+            raise Exception("ValueError")
+        self.x, self.y, self.s, self.m = xs, ys, ss, ms
+
+    def cm_x(self):
+        return self.x
+
+    def cm_y(self):
+        return self.y
+
+    def mass(self):
+        return self.m
+
+    def m_inert(self):
+        return (self.m * self.s**2)/12
