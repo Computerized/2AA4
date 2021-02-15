902544d7fd3d0a680aca449f2c1612d899bad8bb
Alan Scott <scotta30@mcmaster.ca>
2021-02-15 18:29:40 -0500

    updated plot.py

--- A2/src/Plot.py
+++ A2/src/Plot.py
@@ -4,9 +4,28 @@
 #  @date
 #  @details
 
-import matplotlib
+import matplotlib.pyplot as plt
 
-def plot(w, t):
-    if len(w) != len(t):
-        raise ValueError
-    xvalues, yvalues = w[0], w[1]
+def plot(w,t):
+    x = []
+    y = []
+    for item in w:
+        x.append(item[0])
+        y.append(item[1])
+    plt.figure("Motion Simulation")
+    plt.subplot(311)
+    plt.xlabel("x(t)")
+    plt.ylabel("t")
+    plt.plot(x,t)
+    plt.subplot(312)
+    plt.xlabel("y(t)")
+    plt.ylabel("t")
+    plt.plot(y,t)
+    plt.subplot(313)
+    plt.xlabel("x(t)")
+    plt.ylabel("y(t)")
+    plt.plot(x, y)
+
+    plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.2, hspace=0.8)
+
+    plt.show()
