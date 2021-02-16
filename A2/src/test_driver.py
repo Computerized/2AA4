021f64674e21578fe7060b8e2a38196c40dcf333
Alan Scott <scotta30@mcmaster.ca>
2021-02-16 14:07:54 -0500

    updated test_driver.py

--- A2/src/test_driver.py
+++ A2/src/test_driver.py
@@ -1,9 +1,11 @@
 ## @file test_All.py
-#  @author
-#  @brief
-#  @date
-#  @details
+#  @author Alan Scott
+#  @brief Test file
+#  @date 16/02/21
+#  @details This file tests the output of each function against the expected value.
+#           This is done using pytest, which keeps track of the cases that pass and fail.
 
+import pytest
 from CircleT import CircleT
 from TriangleT import TriangleT
 
@@ -30,10 +32,18 @@ def test_triangle_cmy():
     assert triangle.cm_y() == -10.0
 
 def test_triangle_mass():
-    assert triangle.mass() == 5
+    assert triangle.mass() == 17.5
 
 def test_triangle_inert():
     assert triangle.m_inert() == (17.5*5**2)/12
+
+def test_triangle_except():
+    with pytest.raises(ValueError):
+        TriangleT(1,1,-1,-1)
+
+
+
+
 
 
 
