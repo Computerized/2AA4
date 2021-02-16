a4fe7543007c69c9a38ef70c7225e2f82cddb4ef
Alan Scott <scotta30@mcmaster.ca>
2021-02-16 00:43:31 -0500

    updated files

--- A2/src/Scene.py
+++ A2/src/Scene.py
@@ -13,16 +13,39 @@ class Scene(Shape):
     def __init__(self, sprime, fxprime, fyprime, vxprime, vyprime):
         self.s, self.fx, self.fy, self.vx, self.vy = sprime, fxprime, fyprime, vxprime, vyprime
 
+        total = 0
+    g = 9.81  # accel due to gravity (m/s^2)
+    m = 1  # mass (kg)
+
+    def Fx(self,t):
+        return 5 if t < 5 else 0
+
+
+    def Fy(self,t):
+        return -self.g * self.m if t < 3 else self.g * self.m
+
+    def cm_x(self):
+        pass
+
+    def cm_y(self):
+        pass
+
+    def mass(self):
+        pass
+
+    def m_inert(self):
+        pass
+
     def get_shape(self):
         return self.s
 
     def get_unbal_forces(self):
         return self.fx, self.fy
 
-    def get_init_velocity(self):
+    def get_init_velo(self):
         return self.vx, self.vy
 
-    def set_shapes(self, sprime):
+    def set_shape(self, sprime):
         self.s = sprime
 
     def set_unbal_forces(self, fxprime, fyprime):
@@ -39,6 +62,6 @@ class Scene(Shape):
         return t, out
 
     def ode(self, w, t):
-        return [w[2], w[3], self.fx[t]/self.s.mass, self.fy[t]/self.s.mass]
+        return [w[2], w[3], self.Fx(t)/self.s.mass(), self.Fy(t)/self.s.mass()]
 
 
--- A2/src/test_driver.py
+++ A2/src/test_driver.py
@@ -5,27 +5,35 @@
 #  @details
 
 from CircleT import CircleT
-
-total = 0
-g = 9.81  # accel due to gravity (m/s^2)
-m = 1  # mass (kg)
-
-def Fx(t):
-    return 5 if t < 5 else 0
-
-
-def Fy(t):
-    return -g * m if t < 3 else g * m
+from TriangleT import TriangleT
 
 circle = CircleT(1.0, 10.0, 0.5, 1.0)
 
-def test_circle():
+def test_circle_cmx():
     assert circle.cm_x() == 1.0
+
+def test_circle_cmy():
     assert circle.cm_y() == 10.0
+
+def test_circle_mass():
     assert circle.mass() == 1.0
+
+def test_circle_inert():
     assert circle.m_inert() == (1.0*0.5**2)/2
 
+triangle = TriangleT(1.0, -10.0, 5, 17.5)
+
+def test_triangle_cmx():
+    assert triangle.cm_x() == 1.0
+
+def test_triangle_cmy():
+    assert triangle.cm_y() == -10.0
+
+def test_triangle_mass():
+    assert triangle.mass() == 5
+
+def test_triangle_inert():
+    assert triangle.m_inert() == (17.5*5**2)/12
 
 
-test_circle()
 
