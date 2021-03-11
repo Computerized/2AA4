519957bf1cf8b872e5abc46df1e0eb7c4b45a855
Alan Scott <scotta30@mcmaster.ca>
2021-03-11 16:18:58 -0500

    updated LOsT.java and Norm.java

--- A3/src/LOsT.java
+++ A3/src/LOsT.java
@@ -1,1 +1,10 @@
 package src;
+
+public class LOsT extends Measures{
+	String name;
+	int n_blw, n_mrg, n_mts, n_exc;
+	
+	public LOsT(String topic, int nblw, int nmrg, int nmts, int nexc) {
+		
+	}
+}
--- A3/src/Norm.java
+++ A3/src/Norm.java
@@ -1,1 +1,35 @@
 package src;
+
+public class Norm{
+	boolean normLOs, normInd, normAtt;
+	
+	public void setNorms(boolean nLOs, boolean nInd, boolean nAtt) {
+		normLOs = nLOs;
+		normInd = nInd;
+		normAtt = nAtt;
+	}
+	
+	public boolean getNLOs() {
+		return normLOs;
+	}
+	
+	public boolean getNInd() {
+		return normInd;
+	}
+	
+	public boolean getNAtt() {
+		return normAtt;
+	}
+	
+	public void setNLOs(boolean nLOs) {
+		normLOs = nLOs;
+	}
+	
+	public void NInd(boolean nInd) {
+		normInd = nInd;
+	}
+	
+	public void setNAtt(boolean nAtt) {
+		normAtt = nAtt;
+	}
+}
