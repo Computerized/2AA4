4d99d307ab589ea9a94a4a5616056e424d0fe923
Alan Scott <scotta30@mcmaster.ca>
2021-03-11 16:26:30 -0500

    Update LOsT.java

--- A3/src/LOsT.java
+++ A3/src/LOsT.java
@@ -5,6 +5,10 @@ public class LOsT extends Measures{
 	int n_blw, n_mrg, n_mts, n_exc;
 	
 	public LOsT(String topic, int nblw, int nmrg, int nmts, int nexc) {
-		
+		name = topic;
+		n_blw = nblw;
+		n_mrg = nmrg;
+		n_mts = nmts;
+		n_exc = nexc;
 	}
 }
