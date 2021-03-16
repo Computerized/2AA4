9da4047ed5937e6134db2f554b325f988207ee30
Alan Scott <scotta30@mcmaster.ca>
2021-03-16 19:58:35 -0400

    Update TestCourseT.java

--- A3/src/TestCourseT.java
+++ A3/src/TestCourseT.java
@@ -10,13 +10,24 @@ package src;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 public class TestCourseT
 {
+	private final String courseName = "Course Name";
+	private final IndicatorT [] inds = {IndicatorT.engInSoc, IndicatorT.openEnded, IndicatorT.tools};
+	private CourseT course;
+	
+	@Before
+	public void setUp() {
+		course = new CourseT(courseName, inds);
+	}
 
 	@Test
-    public void testBlank()
+    public void testGetIndicators()
     {
-        assertTrue(true);
+        IndicatorT [] indsOut = course.getIndicators();
+        assertTrue(Arrays.equals(indsOut, inds));
     }
 
 }
