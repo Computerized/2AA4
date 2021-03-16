d859c2228d681af28ef9e07c7aaf4d18fa5e5c7c
Alan Scott <scotta30@mcmaster.ca>
2021-03-16 18:30:14 -0400

    Update TestAttributeT.java

--- A3/src/TestAttributeT.java
+++ A3/src/TestAttributeT.java
@@ -12,11 +12,36 @@ import static org.junit.Assert.*;
 
 public class TestAttributeT
 {
+	private AttributeT att0,att1;
+	private IndicatorT[] inds = {IndicatorT.desPrinciples, IndicatorT.desProcess};
 
+	@Before
+	public void setUp() {
+		att0 = new AttributeT("",new IndicatorT[0]);
+		att1 = new AttributeT("Ind",inds);
+	}
+	
     @Test
-    public void testBlank()
+    public void testEmptyGetName()
     {
-        assertTrue(true);
+    	
+        assertTrue(att0.getName().equals(""));
+    }
+    
+    @Test
+    public void testGetName() {
+    	assertTrue(att1.getName().equals("Ind"));
+    }
+    
+    @Test
+    public void testEmptyInds() {
+    	assertTrue(att0.getIndicators().length == 0);
+    }
+    
+    @Test
+    public void testGetInds() {
+    	IndicatorT[] ins = att1.getIndicators();
+    	assertTrue(ins[0] == IndicatorT.desPrinciples && ins[1] == IndicatorT.desProcess);
     }
 
 }
