914482efda2ffa1ed6658d94e62c5e3eb12a6b2c
Alan Scott <scotta30@mcmaster.ca>
2021-03-30 00:45:14 -0400

    did shiftLeft() and shiftRight()

--- 2048/src/ActiveBoard.java
+++ 2048/src/ActiveBoard.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class ActiveBoard extends Board{
 
@@ -12,11 +13,11 @@ public class ActiveBoard extends Board{
 					return true;
 				else if (row >= 1 && grid[row][column] == grid[row-1][column])
 					return true;
-				else if (row <= 3 && grid[row][column] == grid[row+1][column])
+				else if (row <= 2 && grid[row][column] == grid[row+1][column])
 					return true;
 				else if (column >= 1 && grid[row][column] == grid[row][column-1])
 					return true;
-				else if (column <= 3 && grid[row][column] == grid[row][column+1])
+				else if (column <= 2 && grid[row][column] == grid[row][column+1])
 					return true;
 			}
 		}
@@ -26,35 +27,112 @@ public class ActiveBoard extends Board{
 	public boolean canMove(char direction) {
 		switch(direction) {
 		case 'D':
-			for (int row = 0; row < 3; row++) {
-				for (int column = 0; column < 4; column ++) {
-					if (grid[row][column] == grid[row+1][column])
-						return true;
-					else if (grid[row][column] != 0 && grid[row+1][column] == 0)
-						return true;
-				}
-			}
 			break;
 		case 'U':
-			for (int row = 3; row > 0; row --) {
-				for (int column = 0; column < 4; column ++) {
-					if (grid[row][column] == grid[row-1][column])
-						return true;
-					else if (grid[row][column] != 0 && grid[row-1][column] == 0) {
-						return true;
-					}
-				}
-			}
 			break;
 		case 'L':
-			for (int row = 0; row < 3; row++) {
-				
-			}
 			break;
-			
+		case 'R':
+			break;
 		}
 		
 		return false;
+	}
+	
+	public void shiftLeft() {
+		for (int[] row : grid) {
+			int[] blankRow = {0,0,0,0};
+			if (Arrays.equals(blankRow, row))
+				continue;
+			if (row[0] == row[1] && row[0] != 0) {
+				row[0] = row[0] + row[1];
+				row[1] = 0;
+				if (row[2] == row[3] && row[2] != 0) {
+					row[2] = row[2] + row[3];
+					row[3] = 0;
+				}
+			} else if (row[0] == row[2] && row[0]!=0 && row[1] == 0) {
+				row[0] = row[0] + row[2];
+				row[2] = 0;
+			} else if (row[1] == row[2] && row[1] != 0) {
+				row[1] = row[1] + row[2];
+				row[2] = 0;
+			} else if (row[1] == row[3] && row[1] != 0 && row[2] == 0) {
+				row[1] = row[1] + row[3];
+				row[3] = 0;
+			} else if (row[2] == row[3] && row[2] != 0) {
+				row[2] = row[2] + row[3];
+				row[3] = 0;
+			}
+			
+			for (int k = 0; k < 2; k ++) {
+				for (int i = 0; i < 4; i ++) {
+					if (row[i] == 0) {
+						for (int j = i; j < 3; j ++) {
+							row[j] = row[j+1];
+						}
+						row[3] = 0;
+					}
+				}
+			}
+		}
+	}
+	
+	public void shiftRight() {
+		for (int[] row : grid) {
+			int[] blankRow = {0,0,0,0};
+			if (Arrays.equals(blankRow, row))
+				continue;
+			if (row[3] == row[2] && row[2] != 0) {
+				row [3] = row[2] + row[3];
+				row[2] = 0;
+				if (row[1] == row[0] && row[1] != 0) {
+					row[1] = row[0] + row[1];
+					row[0] = 0;
+				}
+			} else if (row[1] == row[0] && row[0] != 0) {
+				row[1] = row[0];
+				row[0] = 0;
+			} else if (row[3] == row[1] && row[2] == 0 && row[3] != 0) {
+				row[3] = row[3] + row[1];
+				row[1] = 0;
+			} else if (row[2] == row[0] && row[1] == 0 && row[2] != 0) {
+				row[2] = row[2] + row[0];
+				row[0] = 0;
+			} else if (row[1] == row[0] && row[0] != 0) {
+				row[1] = row[1] + row[0];
+				row[0] = 0;
+			}
+			
+			for (int k = 0; k < 2; k++) {
+				for (int i = 3; i > 0; i --) {
+					if (row[i] == 0) {
+						for (int j = i; j > 0; j --) {
+							row[j] = row[j-1];
+						}
+						row[0] = 0;
+					}
+				}
+			}
+		}
+	}
+	
+	public void shiftUp() {
+		
+	}
+	
+	public void shiftDown() {
+		
+	}
+	
+	public static void main(String[] args) {
+		int[][] nums = {{2,2,2,2},{2,0,2,0},{2,0,2,2},{0,2,0,2}};
+		int[][] nums2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		ActiveBoard AB = new ActiveBoard(nums);
+		ActiveBoard AB2 = new ActiveBoard(nums2);
+		System.out.println(AB);
+		AB.shiftRight();
+		System.out.println(AB);
 	}
 
 }
--- 2048/src/testclass.java
+++ 2048/src/testclass.java
@@ -1,12 +1,15 @@
-import java.util.concurrent.TimeUnit;
-
 public class testclass {
 	
 	public static void drawNums(int [][] nums) {
 		
 	}
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+		int [][] nums = {{1,0,1,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+		ActiveBoard AB = new ActiveBoard(nums);
+		GUI gui = new GUI(AB);
+		Thread.sleep(2000);
+		AB.shiftLeft();
+		gui.updateTiles(AB);
 	}
 }
