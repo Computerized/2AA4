8da7a75566ce6259329448ad2d679cfc616ed4b0
Alan Scott <scotta30@mcmaster.ca>
2021-04-01 21:24:36 -0400

    added testing files

---
+++ 2048/src/AllTests.java
@@ -0,0 +1,19 @@
+/**
+ * Author: author
+ * Revised: date
+ * 
+ * Description: 
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   TestBoard.class,	
+   TestActiveBoard.class
+})
+
+public class AllTests
+{
+}
---
+++ 2048/src/TestBoard.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestBoard {
+	private final int [][] empty = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void getTest() {
+		
+	}
+}
