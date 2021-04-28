abf2ecae7a74ef683cf0dce840060dead5a5b999
Alan Scott <scotta30@mcmaster.ca>
2021-04-28 15:59:27 -0400

    Update Seq1D.java

--- Exam Practice/src/Seq1D.java
+++ Exam Practice/src/Seq1D.java
@@ -5,4 +5,16 @@ import java.util.Comparator;
 public class Seq1D<T extends Comparable<T>>{
 	private ArrayList<T> s = new ArrayList<T>();
 	TieHandler tieHandler;
+	
+	Seq1D(ArrayList<T> S, TieHandler t){
+		s = S; tieHandler = t;
+	}
+	
+	void setTieHandler(TieHandler t) {
+		tieHandler = t;
+	}
+	
+	double rank (ArrayList<T> p) {
+		
+	}
 }
