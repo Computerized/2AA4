77a2c87da5aa16d96b4f11ce7b89a4e4dad083af
Alan Scott <scotta30@mcmaster.ca>
2021-04-28 20:51:24 -0400

    pushed code

--- FinalExam/src/HarmonicMean.java
+++ FinalExam/src/HarmonicMean.java
@@ -1,1 +1,16 @@
 package src;
+
+import java.util.ArrayList;
+
+public class HarmonicMean implements MeanCalculator{
+
+	@Override
+	public Double meanCalc(ArrayList<Double> v) {
+		Double hsum = 0.0;
+		for (int i = 0; i < v.size(); i++) {
+			hsum += 1/v.get(i);
+		}
+		return v.size()/hsum;
+	}
+	
+}
